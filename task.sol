20c96bf58c2c21f30dc82a231feb87bf21b93b0b
JawwadAshfaq <108620907+JawwadAshfaq@users.noreply.github.com>
2022-12-06 21:39:33 +0500

    Create task.sol

---
+++ task.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^ 0.8.17;
+
+contract FirstTask{
+    string public Name = "Jawwad";
+    string public Fname;
+    uint public Age = 20;
+    bool public Bydefault;
+    bool public Value = true;
+    mapping(uint => address) private x6;
+    address public x4;
+
+    constructor() {
+        Fname = "Ashfaq";
+    } 
+}
+
+// Constructor us tym p run hota h jis tym hum contract ko deploy krty hain 
+
